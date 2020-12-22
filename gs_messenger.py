8fec8d2d33994378ae1a7bd79b48af12ea8eb729
ghostwalkr <46386104+ghostwalkr@users.noreply.github.com>
2020-12-22 11:57:19 -0600

    Updated GS Messenger
    Added sending/replying to PM functions. Added fetcher function to get new PMs in the background every 3 minutes.

--- gs_messenger.py
+++ gs_messenger.py
@@ -3,10 +3,11 @@ import requests
 import sys
 import cmd
 from bs4 import BeautifulSoup
-from re import compile
+from re import compile, findall
 from getpass import getpass
 from pprint import pprint
 from time import sleep
+from threading import Thread
 
 RESET = '\x1b[39m'
 RED = '\x1b[31m'
@@ -34,15 +35,22 @@ class GSBot:
     def __init__(self):
         self.session = None
         self.logged_in = False
+        self.post_key = ""
         self.pmlist = []
+    
+    def fetcher(self):
+        while True:
+            self.getPms(verbose=False)
+            sleep(180) # 3 minutes
 
-    def login(self, username, password):
+    def login(self, username, password, startFetcher=True):
         self.session = requests.Session()
         response = self.session.get("https://greysec.net")
         # Search the HTML for the "my_post_key" value. It's used in MyBB sessions and required to login.
         html = BeautifulSoup(response.text, "html.parser")
         results = html.find(attrs={"name": "my_post_key"})
         my_post_key = results.attrs["value"]
+        self.post_key = my_post_key
 
         # HTTP POST to log in
         postData = {
@@ -62,6 +70,7 @@ class GSBot:
         else:
             self.logged_in = True
             return
+
     def logout(self):
         response = self.session.get("https://greysec.net/index.php")
         html = BeautifulSoup(response.text, "html.parser")
@@ -69,10 +78,10 @@ class GSBot:
         self.session.get("https://greysec.net/" + logout_link)
         self.logged_in = False
 
-    def getPms(self, page=1):
+    def getPms(self, page=1, verbose=True):
         mid = 0
         private_messages = []
-        # {pm_title:example, pm_sender:example, pm_contents}
+        # {title:example, sender:example, contents:example, isUnread:true}
         if page == 1:
             response = self.session.get("https://greysec.net/private.php")
         else:
@@ -81,28 +90,70 @@ class GSBot:
         # Find PMs
         html = BeautifulSoup(response.text, "html.parser")
         
-        for result in html.find_all("a", class_=["old_pm", "new_pm"]):
+        results = [result for result in html.find_all("a", class_=["old_pm", "new_pm"])]
+        for result in results:
             pm = {}
+            if result.attrs["class"][0] == "new_pm":
+                pm["isunread"] = True
+            elif result.attrs["class"][0] == "old_pm":
+                pm["isunread"] = False
+            else:
+                pm["isunread"] = "Unknown"
+
             pm["msgid"] = mid
             pm["title"] = result.text
             nextTd = result.find_next("td")
             pm["sender"] = nextTd.text
             pmUrl = result.attrs["href"]
-            pmPage = self.session.get("https://greysec.net/" + pmUrl).text
-            pmHtml = BeautifulSoup(pmPage, "html.parser")
+            pmPage = self.session.get("https://greysec.net/" + pmUrl)
+            pm["pmid"] = findall("pmid=\d{1,7}", pmPage.url)[0].strip("pmid=")
+            pmHtml = BeautifulSoup(pmPage.text, "html.parser")
             pm["contents"] = pmHtml.find("div", class_="post_body scaleimages").text
+            # Show progress of message retrieval
+            if verbose:
+                if (mid + 1) % 2 == 0:
+                    percentage = round((mid + 1) / len(results) * 100, 1)
+                    print(f"Retrieved: {percentage}%")
+
             private_messages.append(pm)
             mid += 1
 
         self.pmlist = private_messages
+        if verbose:
+            print("Retrieval complete")
+
+    def sendPm(self, subject, msg, recipient, pmid=0):
+        # Only specify pmid if you're replying to a message. Not for composing new messages.
+        if bot.logged_in:
+            # Get my_post_key
+            response = self.session.get("https://greysec.net/private.php")
+            pmPage = BeautifulSoup(response.text, "html.parser")
+            self.post_key = pmPage.find("input", attrs={"name":"my_post_key"}).attrs["value"]
+            postData = {
+                    "my_post_key":self.post_key,
+                    "to": recipient,
+                    "bcc": "",
+                    "subject": subject,
+                    "action": "do_send",
+                    "pmid": pmid,
+                    "do": "reply",
+                    "options[signature]": "0",
+                    "options[savecopy]": "1",
+                    "options[readreceipt]": "1",
+                    "message": msg
+                    }
+            response = self.session.post("https://greysec.net/private.php", data=postData)
+            if response.status_code == 200:
+                return True
+            else:
+                return False
 
 class CommandInterpreter(cmd.Cmd):
     prompt = f"{BLUE}[GS Messenger]{RESET} > "
     file = None
+    ruler = "#"
+    doc_header = "GS Messenger Commands:"
     
-    def postloop(self):
-        print("\nGoodbye friend")
-
     def do_login(self, arg):
         "Log into your GreySec account"
         while True:
@@ -116,41 +167,149 @@ class CommandInterpreter(cmd.Cmd):
             if bot.logged_in:
                 print(f"{GREEN}Login Successful{RESET}")
                 break
+        print("Retrieving PMs")
+        bot.getPms()
+        # Start fetcher
+        fetcher = Thread(target=bot.fetcher)
+        fetcher.start()
 
     def do_retrieve(self, arg=1):
-        "Get PMs from GreySec. showpms <pagenumber>"
-        print("Retrieving PMs...")
-        bot.getPms(page=arg)
-        #for message in bot.pmlist:
-        #    print(f"{message['msgid']} {YELLOW}{message['sender']}{RESET} - {BLUE}{message['title']}{RESET}")
+        "Get PMs from GreySec. retrieve <pagenumber> (automatically done when program starts)"
+        if bot.logged_in:
+            print("Retrieving PMs...")
+            bot.getPms(page=arg)
+            #for message in bot.pmlist:
+            #    print(f"{message['msgid']} {YELLOW}{message['sender']}{RESET} - {BLUE}{message['title']}{RESET}")
+        else:
+            print("Not logged in. Use 'login' to login.")
     
     def do_list(self, arg):
         "List the retrieved PMs"
-        if len(bot.pmlist) > 0:
-            for message in bot.pmlist:
-                print(f"{message['msgid']} {YELLOW}{message['sender']}{RESET} - {BLUE}{message['title']}{RESET}")
+        if bot.logged_in:
+            if len(bot.pmlist) > 0:
+                print(f"{YELLOW}Messages\n{RED}*{RESET} = New message")
+                for message in bot.pmlist:
+                    if message["isunread"]:
+                        print(f"{message['msgid']} {YELLOW}{message['sender']}{RESET} - {RED}*{RESET}{BLUE}{message['title']}{RESET}")
+                    else:
+                        print(f"{message['msgid']} {YELLOW}{message['sender']}{RESET} - {BLUE}{message['title']}{RESET}")
+            else:
+                print("{RED}No messages found{RESET}. Try downloading some with 'retrieve'")
         else:
-            print("{RED}No messages found{RESET}. Try downloading some with 'retrieve'")
+            print("Not logged in. Use 'login' to login.")
 
 
     def do_read(self, arg):
         "Read a PM. read <int>"
-        found = False
-        for p in bot.pmlist:
-            if p["msgid"] == int(arg.strip()):
-                found = True
-                print("\n" + "=" * 20)
-                print(p["title"])
-                print("=" * 20)
-                print(p["sender"])
-                print("=" * 20)
-                print(p["contents"])
-        if not found:
-            print("Message not found")
+        if bot.logged_in:
+            found = False
+            for p in bot.pmlist:
+                if p["msgid"] == int(arg.strip()):
+                    found = True
+                    print("\n" + "=" * 20)
+                    print(p["title"])
+                    print("=" * 20)
+                    print(p["sender"])
+                    print("=" * 20)
+                    print(p["contents"])
+            if not found:
+                print("Message not found")
+        else:
+            print("Not logged in. Use 'login' to login.")
+    
+    def do_reply(self, arg):
+        "Reply to a PM. Example: reply 11"
+        if bot.logged_in: # Check that bot is logged in
+            found = False
+            reply_msg = ""
+
+            for p in bot.pmlist: # Find the PM the user wants to reply to
+                if p["msgid"] == int(arg.strip()):
+                    print("\n" + "=" * 20)
+                    print(p["title"])
+                    print("=" * 20)
+                    print(p["sender"])
+                    print("=" * 20)
+                    print(p["contents"])
+
+                    # Prompt user to reply
+                    print("Enter your message. When you are done, type EOF and press enter\n")
+                    while True:
+                        try:
+                            userinput = input("> ")
+                        except KeyboardInterrupt:
+                            print("\nCancelled!")
+                            break
+                        if len(userinput) == 3 and userinput == "EOF":
+                            # Send PM
+                            subject = "RE:" + p["title"]
+                            recipient = p["sender"]
+                            msgPmid = p["pmid"]
+                            print("Sending PM...")
+
+                            successful = bot.sendPm(subject, reply_msg, recipient, pmid=msgPmid)
+                            if successful:
+                                print("PM sent successfully")
+                            else:
+                                print("Error sending PM")
+                            break
+                        else:
+                            reply_msg += userinput + "\r\n"
+            if not found:
+                print("Message not found")
+
+        else:
+            print("Not logged in. Use 'login' to login.")
+    
+    def do_compose(self, arg):
+        "Compose a PM"
+        if bot.logged_in: # Check that bot is logged in
+            found = False
+            msg = ""
+
+            # Prompt user to reply
+            print("Enter the username of recipient")
+            recipient = input("Recipient: ")
+
+            print("Enter the subject")
+            subject = input("Subject: ")
+            print("Enter your message. When you are done, type EOF and press enter. Press Ctrl + C to cancel.\n")
+            while True:
+                try:
+                    userinput = input("> ")
+                except KeyboardInterrupt:
+                    print("\nCancelled!")
+                    break
+                if len(userinput) == 3 and userinput == "EOF":
+                    # Send PM
+                    print("Sending PM...")
+
+                    successful = bot.sendPm(subject, msg, recipient, pmid=0)
+                    if successful:
+                        print("PM sent successfully")
+                    else:
+                        print("Error sending PM")
+                    break
+                else:
+                    msg += userinput + "\r\n\r\n"
+
+        else:
+            print("Not logged in. Use 'login' to login.")
+
+
+    def do_quit(self, arg):
+        "Exit the program"
+        print("Bye friend!")
+        return
 
 ## Main ####
 
 welcome()
 bot = GSBot()
-CommandInterpreter().cmdloop()
+try:
+    CommandInterpreter().cmdloop()
+except Exception as exception:
+    print("Exception:", exception)
+
+# Make sure to logout
 bot.logout()
