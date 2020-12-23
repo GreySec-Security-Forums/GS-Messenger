f1d1fc426ef19099283661d45a6b3b43c5ff9164
ghostwalkr <46386104+ghostwalkr@users.noreply.github.com>
2020-12-23 13:58:13 -0600

    Update gs_messenger.py
    Added PM timestamps and New PM notifications.

--- gs_messenger.py
+++ gs_messenger.py
@@ -41,15 +41,22 @@ class GSBot:
     def __init__(self):
         self.session = None
         self.logged_in = False
-        self.post_key = ""
+        self.post_key = "" # Value provided in MyBB HTML which is necessary for requesting resources.
+        self.new_pms = False
         self.pmlist = []
     
     def fetcher(self):
+        "Periodically updates the PM list. Intended to run in background thread"
         while True:
             self.getPms(verbose=False)
+            if self.new_pms:
+                print("\nNew Private Message")
+                self.new_pms = False
             sleep(180) # 3 minutes
 
     def login(self, username, password, startFetcher=True):
+        "Authenticate with GreySec"
+        # Returns None if authentication was successful and sets self.logged_in to True, raises exception otherwise.
         self.session = requests.Session()
         response = self.session.get("https://greysec.net")
         # Search the HTML for the "my_post_key" value. It's used in MyBB sessions and required to login.
@@ -78,6 +85,8 @@ class GSBot:
             return
 
     def logout(self):
+        "Log out of Greysec"
+        # Sets self.logged_in to False
         response = self.session.get("https://greysec.net/index.php")
         html = BeautifulSoup(response.text, "html.parser")
         logout_link = html.find("a", href=compile("\w*action=logout\w*")).attrs["href"]
@@ -85,6 +94,9 @@ class GSBot:
         self.logged_in = False
 
     def getPms(self, page=1, verbose=True):
+        "Retrieves PMs from account. Need to be authenticated first."
+        # page: <int> The page number of the PMs to get. Gets just the first page by default. No need to get all the PMs at once.
+        # verbose: <bool> Whether to output info to the user. It's only set to False when used by the fetcher method.
         mid = 0
         private_messages = []
         # {title:example, sender:example, contents:example, isUnread:true}
@@ -94,6 +106,8 @@ class GSBot:
             response = self.session.get("https://greysec.net/private.php", params={"fid":"0", "page":page})
 
         # Find PMs
+        # Example 'pm' dictionary:
+        # {"title": "test", "isunread":"True", "msgid": "3", "sender":"Friend", "pmid":"2377", "contents":"hello world"}
         html = BeautifulSoup(response.text, "html.parser")
         
         results = [result for result in html.find_all("a", class_=["old_pm", "new_pm"])]
@@ -101,26 +115,39 @@ class GSBot:
             pm = {}
             if result.attrs["class"][0] == "new_pm":
                 pm["isunread"] = True
+                self.new_pms = True
             elif result.attrs["class"][0] == "old_pm":
                 pm["isunread"] = False
             else:
                 pm["isunread"] = "Unknown"
-
+            
+            # mid: message id. used by the command interpreter to index the messages
             pm["msgid"] = mid
             pm["title"] = result.text
             nextTd = result.find_next("td")
             pm["sender"] = nextTd.text
             pmUrl = result.attrs["href"]
             pmPage = self.session.get("https://greysec.net/" + pmUrl)
+            # pmid: used by greysec for replying to PMs
             pm["pmid"] = findall("pmid=\d{1,7}", pmPage.url)[0].strip("pmid=")
             pmHtml = BeautifulSoup(pmPage.text, "html.parser")
             pm["contents"] = pmHtml.find("div", class_="post_body scaleimages").text
+            
+            pmDate = pmHtml.find(class_="post_date")
+            try:
+                pmRecvdDate = findall("\d{1,2}-\d{1,2}-\d{4}", str(pmDate))[0]
+                pmRecvdTime = findall("\d{1,2}:\d{1,2} [AP]M", pmDate.text)[0]
+            except:
+                pmRecvdDate, pmRecvdTime = findall("\d{2}-\d{2}-\d{4}, \d{2}:\d{2} [AP]M", str(pmDate))[0].split(",")
+                
+            pm["timestamp"] = f"{pmRecvdDate} {pmRecvdTime}"
+
             # Show progress of message retrieval
             if verbose:
                 if (mid + 1) % 2 == 0:
                     percentage = round((mid + 1) / len(results) * 100, 1)
                     print(f"Retrieved: {percentage}%")
-
+            
             private_messages.append(pm)
             mid += 1
 
@@ -129,9 +156,16 @@ class GSBot:
             print("Retrieval complete")
 
     def sendPm(self, subject, msg, recipient, pmid=0):
+        "Send a PM to a user"
+        # subject <string> - the subject of the message.
+        # msg <string> - the message to send
+        # recipient <string> - username of account to send PM to.
+        # pmid <int> - The PM id we're replying to, or 0 if composing new message
+        # returns: True on successful sending, False otherwise.
         # Only specify pmid if you're replying to a message. Not for composing new messages.
         if bot.logged_in:
             # Get my_post_key
+            # Need to get new post key. Greysec was complaining about the program using the original post key used for logging in for some reason.
             response = self.session.get("https://greysec.net/private.php")
             pmPage = BeautifulSoup(response.text, "html.parser")
             self.post_key = pmPage.find("input", attrs={"name":"my_post_key"}).attrs["value"]
@@ -149,6 +183,7 @@ class GSBot:
                     "message": msg
                     }
             response = self.session.post("https://greysec.net/private.php", data=postData)
+            # The following isn't going to handle errors very well. Further testing is required to determine what could go wrong so it can be handled.
             if response.status_code == 200:
                 return True
             else:
@@ -162,6 +197,8 @@ class CommandInterpreter(cmd.Cmd):
     
     def do_login(self, arg):
         "Log into your GreySec account"
+        if bot.logged_in: print("You're already logged in"); return
+
         while True:
             username = input("Username: ").strip()
             password = getpass("Password: ")
@@ -178,6 +215,9 @@ class CommandInterpreter(cmd.Cmd):
         # Start fetcher
         fetcher = Thread(target=bot.fetcher)
         fetcher.start()
+
+        # change prompt
+        self.prompt = f"{BLUE}[{username}@greysec.net]{RESET} > "
 
     def do_retrieve(self, arg=1):
         "Get PMs from GreySec. retrieve <pagenumber> (automatically done when program starts)"
@@ -212,11 +252,13 @@ class CommandInterpreter(cmd.Cmd):
             for p in bot.pmlist:
                 if p["msgid"] == int(arg.strip()):
                     found = True
-                    print("\n" + "=" * 20)
-                    print(p["title"])
-                    print("=" * 20)
-                    print(p["sender"])
-                    print("=" * 20)
+                    print("\n" + "=" * 40)
+                    print("Subject:", p["title"])
+                    print("=" * len(p["title"]))
+                    print("Sender:", p["sender"])
+                    print("=" * len(p["title"]))
+                    print("Timestamp:", p["timestamp"])
+                    print("=" * len(p["timestamp"]))
                     print(p["contents"])
             if not found:
                 print("Message not found")
@@ -306,7 +348,7 @@ class CommandInterpreter(cmd.Cmd):
     def do_quit(self, arg):
         "Exit the program"
         print("Bye friend!")
-        bye()
+        sys.exit(0)
 
 ## Main ####
 
